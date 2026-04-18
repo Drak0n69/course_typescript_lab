78c68848ea29ccf17a7efe915b54fd772e319e95
DenisVyrezkov <dvyrezkov@vtb.ru>
2026-04-18 16:03:51 +0600

    Кейс: basic34

--- src/basic/basic34.js
+++ src/basic/basic34.js
@@ -3,8 +3,13 @@
 */
 
 export function isSymmetric(matrix) {
-
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] !== matrix[j][i]) {
+                return false;
+            }
+        }
+    }
+    
     return true;
 }
-
-
