bd293c1097c49772b6ed5dff2f10d1c1d4a96931
DenisVyrezkov <dvyrezkov@vtb.ru>
2026-04-18 16:02:40 +0600

    Кейс: basic32

--- src/basic/basic32.js
+++ src/basic/basic32.js
@@ -3,5 +3,18 @@
 */
 
 export function matrixMultiplication(A, B) {
-  return [[1]];
+  const result = [];
+  
+  for (let i = 0; i < A.length; i++) {
+    result[i] = [];
+    for (let j = 0; j < B[0].length; j++) {
+      let sum = 0;
+      for (let k = 0; k < B.length; k++) {
+        sum += A[i][k] * B[k][j];
+      }
+      result[i][j] = sum;
+    }
+  }
+
+  return result;
 }
