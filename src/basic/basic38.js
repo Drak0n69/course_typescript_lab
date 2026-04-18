d90b1a1b54ee7d8610ca5a57d02ac9f85616e95f
DenisVyrezkov <dvyrezkov@vtb.ru>
2026-04-18 16:13:08 +0600

    Кейс: basic38

--- src/basic/basic38.js
+++ src/basic/basic38.js
@@ -3,8 +3,11 @@
 */
 
 export function some(arr, callback) {
-	return false;
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i])) {
+			return true;
+		}
+	}
+	
+	return false;	
 }
-
-
-
