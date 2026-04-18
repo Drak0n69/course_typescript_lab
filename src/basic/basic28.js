7eb8708cf0b48f54ce1abb5c865b83113a8db6b8
DenisVyrezkov <dvyrezkov@vtb.ru>
2026-04-18 15:59:22 +0600

    Кейс: basic28

--- src/basic/basic28.js
+++ src/basic/basic28.js
@@ -3,6 +3,12 @@
 */
 
 export function removeDuplicates(arr) {
-
-	return arr;
+	const uniqueArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (uniqueArr.indexOf(arr[i]) === -1) {
+			uniqueArr.push(arr[i]);
+		}
+	}
+	
+	return uniqueArr;
 }
